472b72f744ffb983ea31c22e0b89bc2b192d49f2
Harrod Tang <harrodstang@gmail.com>
2024-03-23 14:24:45 -0700

    Update profile photo and resume

--- client/src/Portfolio/Header/Profile/Profile.js
+++ client/src/Portfolio/Header/Profile/Profile.js
@@ -67,7 +67,7 @@ function Profile() {
 
             <Button
               variant="contained"
-              href="harrod-resume-f2023.pdf"
+              href="harrod-resume.pdf"
               target="_blank"
               rel="noopener noreferrer"
               className="btn option-btn resume-btn"
